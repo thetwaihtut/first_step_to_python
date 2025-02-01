e21deebed9eed5ce9927290fbfc6f2fd647b0b71
Thet Wai Htut <71161636+thetwaihtut@users.noreply.github.com>
2025-02-01 15:12:58 +0700

    Delete ifelse.py

--- ifelse.py
+++
@@ -1,9 +0,0 @@
-gender=input('enter your gender:(male or female)=')
-age=int(input('enter your age'))
-if age>=18:
-    if gender=='male':
-        print('u can buy beer')
-    elif gender=='female':
-        print('u can buy beer girl')
-else:
-    print('go home')
